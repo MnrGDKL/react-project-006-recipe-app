7e9c3bfe4644e58d2ae0820566bb8cdbb314c806
MnrGDKL <munirgedikli.2021@gmail.com>
2022-04-08 00:13:23 +0300

    commited

--- src/pages/login/Login.js
+++ src/pages/login/Login.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { useState } from "react";
 import { useNavigate } from 'react-router-dom';
-import { Link } from 'react-router-dom';
 import MealSvg from "../../assets/meal.svg";
 import {MainContainer, LoginContainer, Header, FormContainer} from "./style";
 
